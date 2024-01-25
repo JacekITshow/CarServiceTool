9b308aff81a99ccbb275ac79320979b5897adc51
Jacek <jacek19964@gmail.com>
2024-01-25 19:58:09 +0100

    CST-4
    -create WebConfig class to make possible connectino between frontend and backend with CorsMapping

--- java/src/main/java/pl/itshow/java/WebConfig.java
+++ java/src/main/java/pl/itshow/java/WebConfig.java
@@ -2,14 +2,17 @@ package pl.itshow.java;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig  implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
