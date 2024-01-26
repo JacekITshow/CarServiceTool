06fb77b70766d4fff873b7d522250e6776621e51
Jacek <jacek19964@gmail.com>
2024-01-26 18:08:15 +0100

    CST-5
    -fix connection with backend (enable credentials for cors)
    -change info GetMapping
    -add dependency actuator for checking health of backend application

--- java/src/main/java/pl/itshow/java/InfoController.java
+++ java/src/main/java/pl/itshow/java/InfoController.java
@@ -4,11 +4,13 @@ package pl.itshow.java;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class InfoController {
 
-    @GetMapping("/info/")
+    @GetMapping("/info")
     String info() {
-        return "info";
+        return "Current time is: " + new Date();
     }
 }
